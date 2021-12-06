4f5e8bca719803a650647cf20c739ccab242f687
Ryan Chandler <ryangjchandler@gmail.com>
2021-12-06 18:02:41 +0000

    chore: replace fa icon with heroicon

--- resources/views/components/uploader.blade.php
+++ resources/views/components/uploader.blade.php
@@ -1,8 +1,8 @@
 @props(['trigger' => false, 'dropTarget' => 'body', 'config' => '{}'])
 
 <div class="drop-target absolute inset-0 z-50 bg-gray-500 bg-opacity-75 items-center justify-center">
-    <div class="text-2xl xl:text-4xl text-white font-bold text-center">
-        <i class="fas fa-cloud-upload fa-3x opacity-50 mb-3"></i>
+    <div class="text-2xl xl:text-4xl text-white font-bold flex flex-col items-center">
+        <svg class="w-32 h-32 opacity-50 mb-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 13a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 13H11V9.413l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13H5.5z"></path><path d="M9 13h2v5a1 1 0 11-2 0v-5z"></path></svg>
 
         <div>Drop files to upload</div>
     </div>
