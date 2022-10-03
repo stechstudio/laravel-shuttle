5c77fd214946ebb6a7bf74916aa24b6cc6e7c6a9
HazJ <me@hazjohnson.com>
2022-10-03 08:54:59 +0300

    Set default config values, update .env.example

--- config/shuttle.php
+++ config/shuttle.php
@@ -14,12 +14,12 @@ return [
      * The disk you want to use for file uploads. This
      * must be a disk that uses the S3 driver.
      */
-    'disk' => 's3',
+    'disk'       => 's3',
 
     /**
      * The authentication guard used for authorization.
      */
-    'guard' => 'web',
+    'guard'      => 'web',
 
     /**
      * The background colors used for the file uploader UI.
@@ -30,7 +30,7 @@ return [
      * file. Custom HEX values are not compiled
      * at run time.
      */
-    'colors' => [
+    'colors'     => [
 
         'details-panel' => [
             'uploading' => env(key: 'DETAILS_PANEL_UPLOADING', default: 'bg-blue-500'),
@@ -39,7 +39,7 @@ return [
 
             'upload-error' => env(key: 'DETAILS_PANEL_UPLOAD_ERROR', default: 'bg-red-500'),
 
-            'connection-lost' => env(key: 'DETAILS_PANEL_CONNECTION_LOST', default: 'bg-blue-700'),
+            'connection-lost' => env(key: 'DETAILS_PANEL_CONNECTION_LOST', default: 'bg-gray-500'),
         ],
 
     ],
